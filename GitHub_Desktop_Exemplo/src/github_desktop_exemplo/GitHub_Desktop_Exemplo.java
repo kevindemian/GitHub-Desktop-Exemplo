a16e338cdedd7c8567ce5263d199d3bd606e6e06
Kevin Demian Borges <escrevaprokevin@gmail.com>
2023-04-25 20:54:02 -0300

    Criação do projeto

---
+++ GitHub_Desktop_Exemplo/src/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github_desktop_exemplo;
+
+/**
+ *
+ * @author escre
+ */
+public class GitHub_Desktop_Exemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
