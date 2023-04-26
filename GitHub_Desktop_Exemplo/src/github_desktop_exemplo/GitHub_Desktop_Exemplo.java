dd25d97500dcb6ab50c69af1faad2c6680473742
Kevin Demian Borges <escrevaprokevin@gmail.com>
2023-04-25 21:17:48 -0300

    Hello, World!

--- GitHub_Desktop_Exemplo/src/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
+++ GitHub_Desktop_Exemplo/src/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package github_desktop_exemplo;
 
-/**
- *
- * @author escre
- */
 public class GitHub_Desktop_Exemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Hello, World!");
+
     }
     
 }
